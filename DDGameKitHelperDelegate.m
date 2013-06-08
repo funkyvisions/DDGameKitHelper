70bbeef42f5bbf35fca9a16a0a1b4fa2b876bbd1
Dominik Hadl <nick@dynamicdust.com>
2013-06-08 11:30:36 +0200

    Improve readability, convert to ARC and fix typos

--- DDGameKitHelperDelegate.m
+++ DDGameKitHelperDelegate.m
@@ -2,29 +2,48 @@
 //  DDGameKitHelperDelegate.h
 //  Version 1.0
 
+// -----------------------------------------------------------------
+#define DDGAMEKIT_USE_NOTIFICATION 1
+// -----------------------------------------------------------------
+
 #import "DDGameKitHelperDelegate.h"
+#if DDGAMEKIT_USE_NOTIFICATION == 1
 #import "GKAchievementHandler.h"
+#endif
 
+// -----------------------------------------------------------------
 @implementation DDGameKitHelperDelegate
+// -----------------------------------------------------------------
 
-// return true if score1 is greater than score2
-// modify this if your scoreboard is reversed (low scores on top)
--(bool) compare:(int64_t)score1 to:(int64_t)score2
+// Returns 'true' if score1 is greater than score2
+// Modify this if your scoreboard is reversed (lowest scores first)
+// For example -  a lap time in a racer game (the lower the better)
+- (BOOL) compareScore:(int64_t)score1 toScore:(int64_t)score2
 {
     return score1 > score2;
 }
 
-// display new high score using GKAchievement class
--(void) onSubmitScore:(int64_t)score;
+// -----------------------------------------------------------------
+
+// If enabled, display new high score notification using GKAchievementHandler
+- (void) onSubmitScore:(int64_t)score;
 {
-    [[GKAchievementHandler defaultHandler] notifyAchievementTitle:@"New High Score!!!" andMessage:[NSString stringWithFormat:@"%d", score]];
+#if DDGAMEKIT_USE_NOTIFICATION == 1
+    [[GKAchievementHandler defaultHandler] notifyAchievementTitle:@"New High Score!" andMessage:[NSString stringWithFormat:@"%d", (int)score]];
+#endif
 }
 
-// display the achievement using GKAchievement class
--(void) onReportAchievement:(GKAchievement*)achievement
+// -----------------------------------------------------------------
+
+// If enabled, display achievement notification using GKAchievementHandler
+- (void) onReportAchievement:(GKAchievement*)achievement
 {
+#if DDGAMEKIT_USE_NOTIFICATION == 1
     DDGameKitHelper* gkHelper = [DDGameKitHelper sharedGameKitHelper];
     [[GKAchievementHandler defaultHandler] notifyAchievement:[gkHelper getAchievementDescription:achievement.identifier]];
+#endif
 }
 
+// -----------------------------------------------------------------
 @end
+// -----------------------------------------------------------------
